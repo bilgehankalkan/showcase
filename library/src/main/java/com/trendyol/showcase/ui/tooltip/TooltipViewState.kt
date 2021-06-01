5774660b7dae4f3d15fa731fd18e762a73f0ef95
Yasin Arslan <yasin.arslan@trendyol.com>
2021-06-01 09:36:07 +0300

    - define ActionType enum to handle click information - edit TooltipView to pass click info and communicate via listener instead of activity#backPress

--- library/src/main/java/com/trendyol/showcase/ui/tooltip/TooltipViewState.kt
+++ library/src/main/java/com/trendyol/showcase/ui/tooltip/TooltipViewState.kt
@@ -1,54 +1,62 @@
 package com.trendyol.showcase.ui.tooltip
 
 import android.view.View
-import androidx.annotation.ColorInt
-import androidx.annotation.DrawableRes
 import com.trendyol.showcase.R
+import com.trendyol.showcase.showcase.ShowcaseModel
 import com.trendyol.showcase.util.Constants
 
 internal data class TooltipViewState(
-    val titleText: String,
-    val descriptionText: String,
-    @ColorInt val titleTextColor: Int,
-    @ColorInt val descriptionTextColor: Int,
-    @ColorInt val backgroundColor: Int,
-    @ColorInt val closeButtonColor: Int,
-    val showCloseButton: Boolean = true,
-    @DrawableRes val arrowResource: Int,
+    val showcaseModel: ShowcaseModel,
     val arrowPosition: AbsoluteArrowPosition,
-    val arrowPercentage: Int?,
-    val arrowMargin: Int,
-    val titleTextSize: Float,
-    val descriptionTextSize: Float,
-    val textPosition: TextPosition,
-    val imageUrl: String,
-    val showCustomContent: Boolean,
-    val isStatusBarVisible: Boolean
+    val arrowMargin: Int
 ) {
+    fun getBackgroundColor(): Int = showcaseModel.popupBackgroundColor
 
-    fun getImageViewVisibility(): Int = if (imageUrl.isEmpty()) View.GONE else View.VISIBLE
+    fun getImageUrl(): String = showcaseModel.imageUrl
+
+    fun getImageViewVisibility(): Int = if (showcaseModel.imageUrl.isBlank()) View.GONE else View.VISIBLE
 
     fun getTextPosition(): Int {
-        return when (textPosition) {
+        return when (showcaseModel.textPosition) {
             TextPosition.CENTER -> 4
             TextPosition.END -> 3
             else -> 2
         }
     }
 
-    fun getTitleVisibility() = titleText.isNotEmpty()
+    fun getTitle() = showcaseModel.titleText
 
-    fun getDescriptionVisibility() = descriptionText.isNotEmpty()
+    fun getTitleVisibility() = showcaseModel.titleText.isNotEmpty()
 
-    fun getTopArrowResource() = if (arrowResource == Constants.DEFAULT_ARROW_RESOURCE) R.drawable.ic_showcase_arrow_up else arrowResource
+    fun getTitleTextColor() = showcaseModel.titleTextColor
+
+    fun getTitleTextSize() = showcaseModel.titleTextSize
+
+    fun getDescription() = showcaseModel.descriptionText
+
+    fun getDescriptionVisibility() = showcaseModel.descriptionText.isNotEmpty()
+
+    fun getDescriptionTextColor() = showcaseModel.descriptionTextColor
+
+    fun getDescriptionTextSize() = showcaseModel.descriptionTextSize
+
+    fun getArrowPercentage() = showcaseModel.arrowPercentage
+
+    fun getTopArrowResource() =
+        if (showcaseModel.arrowResource == Constants.DEFAULT_ARROW_RESOURCE) R.drawable.ic_showcase_arrow_up else showcaseModel.arrowResource
 
     fun getTopArrowVisibility() = if (arrowPosition == AbsoluteArrowPosition.UP) View.VISIBLE else View.GONE
 
-    fun getBottomArrowResource() = if (arrowResource == Constants.DEFAULT_ARROW_RESOURCE) R.drawable.ic_showcase_arrow_down else arrowResource
+    fun getBottomArrowResource() =
+        if (showcaseModel.arrowResource == Constants.DEFAULT_ARROW_RESOURCE) R.drawable.ic_showcase_arrow_down else showcaseModel.arrowResource
 
     fun getBottomArrowVisibility() = if (arrowPosition == AbsoluteArrowPosition.DOWN) View.VISIBLE else View.GONE
 
-    fun getCloseButtonVisibility() = if (showCloseButton) View.VISIBLE else View.GONE
+    fun getCloseButtonColor() = showcaseModel.closeButtonColor
 
-    fun isContentVisible(): Int = if (showCustomContent) View.GONE else View.VISIBLE
+    fun getCloseButtonVisibility() = if (showcaseModel.showCloseButton) View.VISIBLE else View.GONE
+
+    fun getContentVisibility() = if (showcaseModel.customContent != null) View.GONE else View.VISIBLE
+
+    fun isShowcaseViewClickable() = showcaseModel.isShowcaseViewClickable
 }
